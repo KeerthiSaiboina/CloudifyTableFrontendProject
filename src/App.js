d43812eaa77a35f464251f0f5d8d3e29c1ac20a9
KeerthiSaiboina <164760771+KeerthiSaiboina@users.noreply.github.com>
2025-01-22 15:04:31 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Tab from "./components/Tab";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Dynamic Table with Dropdowns</h1>
+      <Tab />
+    </div>
+  );
+}
+
+export default App;
